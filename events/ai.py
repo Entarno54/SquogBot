c530ab4f2bee1c070c215f5b56fde0a2d6394aa7
= <=>
2025-04-07 18:04:14 +0700

    !kidnap

---
+++ events/ai.py
@@ -0,0 +1,12 @@
+import nextcord
+from nextcord.ext import commands
+import requests
+
+class AI(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.client = bot
+
+    @commands.Cog.listener()
+    async def on_message_create(self, message: nextcord.Message):
+        print(message.content)
+        self.client.process_application_commands(message)
